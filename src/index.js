f53459d810b18397e7eb6c480e535fbcf0c7796c
Garret <gwiats@gmail.com>
2021-03-28 02:21:17 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById("create-task-form").addEventListener('submit', addTask)
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("deleteForm").addEventListener('submit', deleteTask)
+});
+
+function addTask(event) {
+  event.preventDefault()
+  const input = event.target.querySelector('input').value
+  const lists = document.getElementById('tasks')
+  const value = document.createElement('li')
+  value.textContent = input
+  lists.appendChild(value)
+  document.getElementById('new-task-description').value = ""
+}
+
+function deleteTask(event) {
+  event.preventDefault()
+  const input = event.target.querySelector('input').value
+  const num = parseInt(input)
+  const value = document.getElementById('tasks')
+  value.removeChild(value.childNodes[num])
+  document.getElementById('delete description').value = ""
+}
